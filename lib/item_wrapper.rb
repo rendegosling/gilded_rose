74a5f377c5f14200257b58ca42e774342c9cc7cb
renlesterdg <renlesterdeguzman@gmail.com>
2016-11-17 00:05:43 +0800

    group some related tasks and extract

--- lib/item_wrapper.rb
+++ lib/item_wrapper.rb
@@ -2,9 +2,29 @@ require 'delegate'
 
 class ItemWrapper < SimpleDelegator
   def update
+    handle_sell_in
+    handle_quality
+  end
+
+  def increment_quality
+    if quality < 50
+      self.quality += 1
+    end
+  end
+
+  def decrement_quality
+    if quality > 0
+      self.quality -= 1
+    end
+  end
+
+  def handle_sell_in
     if name != 'Sulfuras, Hand of Ragnaros'
       self.sell_in -= 1
     end
+  end
+
+  def handle_quality
     if name != 'Aged Brie' && name != 'Backstage passes to a TAFKAL80ETC concert'
       if name != 'Sulfuras, Hand of Ragnaros'
         decrement_quality
@@ -32,18 +52,6 @@ class ItemWrapper < SimpleDelegator
       else
         increment_quality
       end
-    end
-  end
-
-  def increment_quality
-    if quality < 50
-      self.quality += 1
-    end
-  end
-
-  def decrement_quality
-    if quality > 0
-      self.quality -= 1
     end
   end
 end
