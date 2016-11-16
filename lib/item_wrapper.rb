ceb71166650820a85a2d078336f735d605774f61
renlesterdg <renlesterdeguzman@gmail.com>
2016-11-17 00:01:01 +0800

    remove redundant self.

--- lib/item_wrapper.rb
+++ lib/item_wrapper.rb
@@ -2,32 +2,32 @@ require 'delegate'
 
 class ItemWrapper < SimpleDelegator
   def update
-    if self.name != 'Sulfuras, Hand of Ragnaros'
+    if name != 'Sulfuras, Hand of Ragnaros'
       self.sell_in -= 1
     end
-    if self.name != 'Aged Brie' && self.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if self.name != 'Sulfuras, Hand of Ragnaros'
+    if name != 'Aged Brie' && name != 'Backstage passes to a TAFKAL80ETC concert'
+      if name != 'Sulfuras, Hand of Ragnaros'
         decrement_quality
       end
     else
       increment_quality
-      if self.name == 'Backstage passes to a TAFKAL80ETC concert'
-        if self.sell_in < 10
+      if name == 'Backstage passes to a TAFKAL80ETC concert'
+        if sell_in < 10
           increment_quality
         end
-        if self.sell_in < 6
+        if sell_in < 6
           increment_quality
         end
       end
     end
-    if self.sell_in < 0
-      if self.name != "Aged Brie"
-        if self.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if self.name != 'Sulfuras, Hand of Ragnaros'
+    if sell_in < 0
+      if name != "Aged Brie"
+        if name != 'Backstage passes to a TAFKAL80ETC concert'
+          if name != 'Sulfuras, Hand of Ragnaros'
             decrement_quality
           end
         else
-          self.quality = self.quality - self.quality
+          self.quality = self.quality - quality
         end
       else
         increment_quality
@@ -36,13 +36,13 @@ class ItemWrapper < SimpleDelegator
   end
 
   def increment_quality
-    if self.quality < 50
+    if quality < 50
       self.quality += 1
     end
   end
 
   def decrement_quality
-    if self.quality > 0
+    if quality > 0
       self.quality -= 1
     end
   end
