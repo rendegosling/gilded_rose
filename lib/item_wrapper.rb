319a35fcbd3b7e196bbf0c2ca46dcb5349dd1208
renlesterdg <renlesterdeguzman@gmail.com>
2016-11-16 23:57:29 +0800

    fix a bug when restructuring the intent

--- lib/item_wrapper.rb
+++ lib/item_wrapper.rb
@@ -2,6 +2,9 @@ require 'delegate'
 
 class ItemWrapper < SimpleDelegator
   def update
+    if self.name != 'Sulfuras, Hand of Ragnaros'
+      self.sell_in -= 1
+    end
     if self.name != 'Aged Brie' && self.name != 'Backstage passes to a TAFKAL80ETC concert'
       if self.name != 'Sulfuras, Hand of Ragnaros'
         decrement_quality
@@ -9,16 +12,13 @@ class ItemWrapper < SimpleDelegator
     else
       increment_quality
       if self.name == 'Backstage passes to a TAFKAL80ETC concert'
-        if self.sell_in < 11
+        if self.sell_in < 10
           increment_quality
         end
         if self.sell_in < 6
           increment_quality
         end
       end
-    end
-    if self.name != 'Sulfuras, Hand of Ragnaros'
-      self.sell_in -= 1
     end
     if self.sell_in < 0
       if self.name != "Aged Brie"
